aaa59c40eb0ae77d80af22843d5c8fcf00515ef2
szheng7795 <szheng7795@dbu.edu>
2025-10-22 19:18:23 -0500

    update get data to download to current dir

--- get_data.py
+++ get_data.py
@@ -1,6 +1,19 @@
+import shutil
+
 import kagglehub
+import pandas as pd
 import os
 # Download latest version
-path = kagglehub.dataset_download("hummaamqaasim/jobs-in-data")
+import kagglehub
+import pandas as pd
+import os
 
-print("Path to dataset files:", path)
+# Download latest version
+path = kagglehub.dataset_download("hummaamqaasim/jobs-in-data")
+file_name = [i for i in os.listdir(path) if i.endswith(".csv")][0]
+org_file_path = os.path.join(path, file_name)
+cur_file_path = os.path.join(os.getcwd(), file_name)
+
+shutil.copy(org_file_path, cur_file_path)
+data_df = pd.read_csv(cur_file_path)
+print(data_df)
