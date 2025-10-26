686bc32f8954df1addabbe2b3e2b40cd5cb24f33
szheng7795 <szheng7795@dbu.edu>
2025-10-26 12:46:02 -0500

    update .gitignore to ignore .idea and add comments

--- get_data.py
+++ get_data.py
@@ -13,7 +13,7 @@ path = kagglehub.dataset_download("hummaamqaasim/jobs-in-data")
 file_name = [i for i in os.listdir(path) if i.endswith(".csv")][0]
 org_file_path = os.path.join(path, file_name)
 cur_file_path = os.path.join(os.getcwd(), file_name)
-
+# Copy dataset to current directory
 shutil.copy(org_file_path, cur_file_path)
 data_df = pd.read_csv(cur_file_path)
 print(data_df)
